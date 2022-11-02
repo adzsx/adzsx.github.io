846cf6720cb538449782043fce2460a347e82f49
adzsx <adzsx>
2022-11-02 22:51:24 +0100

    Enter search

--- home/main.js
+++ home/main.js
@@ -11,3 +11,17 @@ button.onclick = function () {
     var full = query + value.replace(" ", "+")
     location.href = full
 }
+
+input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      var value = input.value
+      while(value.charAt(0) === " "){
+          value = value.substring(1)
+      }
+  
+      var query = "https://startpage.com/search?query="
+      var full = query + value.replace(" ", "+")
+      location.href = full
+    }
+  }); 
